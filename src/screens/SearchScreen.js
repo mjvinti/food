f4c570c81784beac7eeb90d9ab1c3defd75d8b84
Mario Vinti <mjvinti@gmail.com>
2022-11-03 16:15:57 -0400

    added use effect hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import SearchBar from "../components/SearchBar";
@@ -10,12 +10,12 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [term, setTerm] = useState("");
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
     try {
       const response = await yelp.get("/search", {
         params: {
           limit: 50,
-          term,
+          term: searchTerm,
           location: "san jose",
         },
       });
@@ -25,9 +25,17 @@ const SearchScreen = () => {
     }
   };
 
+  useEffect(() => {
+    searchApi("pasta");
+  }, []);
+
   return (
     <View>
-      <SearchBar onTermChange={setTerm} onTermSubmit={searchApi} term={term} />
+      <SearchBar
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+        term={term}
+      />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
     </View>
