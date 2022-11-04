3f29580d15c30b625f9924221f278435812ac7b2
Mario Vinti <mjvinti@gmail.com>
2022-11-04 16:20:18 -0400

    flatlist rendering

--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -1,11 +1,16 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 
 const ResultsList = ({ results, title }) => {
   return (
     <View>
       <Text style={styles.title}>{title}</Text>
-      <Text>{results.length}</Text>
+      <FlatList
+        data={results}
+        horizontal
+        keyExtractor={(result) => result.id}
+        renderItem={({ item }) => <Text>{item.name}</Text>}
+      />
     </View>
   );
 };
